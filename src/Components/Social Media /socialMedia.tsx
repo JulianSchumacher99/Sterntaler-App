0d099f5ca01701b752712b362d9d47aef2be626a
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-11-22 18:56:07 +0100

    ✨  Create Social Media Component

---
+++ src/Components/Social Media /socialMedia.tsx
@@ -0,0 +1,37 @@
+import styles from './socialmedia.module.css';
+
+function SocialMedia(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Hey, i like you!</div>
+      <div className={styles.socialmedia}>
+        <img
+          className={styles.img}
+          src="https://cdn.shopify.com/s/files/1/0285/6920/6887/files/strumpfhose_375x.jpg?v=1634277375"
+          alt="Junge in Sterntaler Strumphose"
+        />
+        <a href="https://www.instagram.com/sterntalergmbh/">
+          <img
+            className={styles.img}
+            src="https://cdn.shopify.com/s/files/1/0285/6920/6887/files/Insta_375x.png?v=1615292493"
+            alt="Sterntaler Instagram"
+          />
+        </a>
+        <img
+          className={styles.img}
+          src="https://cdn.shopify.com/s/files/1/0285/6920/6887/files/HW21_c9e9ecc5-7bf6-417c-83bc-44daf3a35dfe_375x.jpg?v=1629436981"
+          alt="Sterntaler Wintermützen"
+        />
+        <a href="https://www.facebook.com/sterntalergmbh/">
+          <img
+            className={styles.img}
+            src="https://cdn.shopify.com/s/files/1/0285/6920/6887/files/Facebook_375x.png?v=1615292535"
+            alt="Sterntaler Facebook"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default SocialMedia;
