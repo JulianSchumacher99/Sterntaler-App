30ad00bb16fd98147e2b0b2bdbf6d9ee6f82f7b0
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-11-24 19:37:57 +0100

    ✨  File Setup for new Component

---
+++ src/Components/Blog Filter/blogFilter.tsx
@@ -0,0 +1,7 @@
+import styles from './blogFilter.module.css';
+
+function BlogFilter(): JSX.Element {
+  return <div className={styles.container}>Hello World</div>;
+}
+
+export default BlogFilter;
---
+++ src/pages/Ratgeber/ratgeber.tsx
@@ -0,0 +1,13 @@
+import BlogFilter from '../../Components/Blog Filter/blogFilter';
+import Blog from '../../Components/Blog/blog';
+
+function Ratgeber(): JSX.Element {
+  return (
+    <>
+      <BlogFilter />
+      <Blog />
+    </>
+  );
+}
+
+export default Ratgeber;
