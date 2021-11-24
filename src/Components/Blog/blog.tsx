13d7e71c4b80426ee5970b620b6efb15ba0fed4e
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-11-24 19:21:51 +0100

    ✨  Create Blog Component

--- src/Components/Blog/blog.tsx
+++ src/Components/Blog/blog.tsx
@@ -1,7 +1,40 @@
 import styles from './blog.module.css';
 
 function Blog(): JSX.Element {
-  return <div className={styles.container}>Hello World 🥳</div>;
+  const buttonText = 'Mehr lesen';
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.contenttext}>
+          <h2 className={styles.h2}>NEW ARRIVALS</h2>
+          <p>Entdecke jetzt die Gatenfreunde Emmily und Emmilius</p>
+          <button className={styles.btn}>{buttonText}</button>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.contenttext}>
+          <h2 className={styles.h2}>Herbst-/ Winterkolletkion</h2>
+          <p>Alle Insights zur neuen Kollektion findest du hier..</p>
+          <button className={styles.btn}>{buttonText}</button>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.contenttext}>
+          <h2 className={styles.h2}>Sterntaler Hebamme</h2>
+          <p>Tipps und Tricks rund um den Baby Schlaf</p>
+          <button className={styles.btn}>{buttonText}</button>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.contenttext}>
+          <h2 className={styles.h2}>Nachhaltigkeit</h2>
+          <p>Wir sind fair, der Umwelt zuliebe!</p>
+          <button className={styles.btn}>{buttonText}</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Blog;
