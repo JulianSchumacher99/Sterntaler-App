48fda153d21934f299b03859806663523c70533a
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-11-25 08:25:10 +0100

    ✨ Basic Funcition Filter

--- src/Components/Blog Filter/blogFilter.tsx
+++ src/Components/Blog Filter/blogFilter.tsx
@@ -1,6 +1,10 @@
 import styles from './blogFilter.module.css';
 
-function BlogFilter(): JSX.Element {
+type FilterProps = {
+  onSetFilter: (filter: string) => void;
+};
+
+function BlogFilter({ onSetFilter }: FilterProps): JSX.Element {
   return (
     <div className={styles.container}>
       <img
@@ -8,8 +12,11 @@ function BlogFilter(): JSX.Element {
         src="https://cdn.shopify.com/s/files/1/0285/6920/6887/t/11/assets/new-book.svg?v=183120823372343621"
         alt=""
       />
-      <div className={styles.filtertext}>Filtern nach:</div>
-      <select className={styles.dropdown}>
+      <p>Filtern nach:</p>
+      <select
+        onChange={(event) => onSetFilter(event.target.value)}
+        className={styles.dropdown}
+      >
         <option>Alle</option>
         <option>Wissenwertes</option>
         <option>Funktionsartikel</option>
--- src/Components/Blog/blog.tsx
+++ src/Components/Blog/blog.tsx
@@ -1,39 +1,46 @@
+import { useState } from 'react';
+import BlogFilter from '../Blog Filter/blogFilter';
 import styles from './blog.module.css';
 
 function Blog(): JSX.Element {
+  const [filter, setFilter] = useState<string>('');
+
   const buttonText = 'Mehr lesen';
 
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.contenttext}>
-          <h2 className={styles.h2}>NEW ARRIVALS</h2>
-          <p>Entdecke jetzt die Gatenfreunde Emmily und Emmilius</p>
-          <button className={styles.btn}>{buttonText}</button>
+    <>
+      <BlogFilter onSetFilter={setFilter} />
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.contenttext}>
+            <h2 className={styles.h2}>NEW ARRIVALS</h2>
+            <p>Entdecke jetzt die Gatenfreunde Emmily und Emmilius</p>
+            <button className={styles.btn}>{buttonText}</button>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contenttext}>
+            <h2 className={styles.h2}>Herbst-/ Winterkolletkion</h2>
+            <p>Alle Insights zur neuen Kollektion findest du hier..</p>
+            <button className={styles.btn}>{buttonText}</button>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contenttext}>
+            <h2 className={styles.h2}>Sterntaler Hebamme</h2>
+            <p>Tipps und Tricks rund um den Baby Schlaf</p>
+            <button className={styles.btn}>{buttonText}</button>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.contenttext}>
+            <h2 className={styles.h2}>Nachhaltigkeit</h2>
+            <p>Wir sind fair, der Umwelt zuliebe!</p>
+            <button className={styles.btn}>{buttonText}</button>
+          </div>
         </div>
       </div>
-      <div className={styles.content}>
-        <div className={styles.contenttext}>
-          <h2 className={styles.h2}>Herbst-/ Winterkolletkion</h2>
-          <p>Alle Insights zur neuen Kollektion findest du hier..</p>
-          <button className={styles.btn}>{buttonText}</button>
-        </div>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.contenttext}>
-          <h2 className={styles.h2}>Sterntaler Hebamme</h2>
-          <p>Tipps und Tricks rund um den Baby Schlaf</p>
-          <button className={styles.btn}>{buttonText}</button>
-        </div>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.contenttext}>
-          <h2 className={styles.h2}>Nachhaltigkeit</h2>
-          <p>Wir sind fair, der Umwelt zuliebe!</p>
-          <button className={styles.btn}>{buttonText}</button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
