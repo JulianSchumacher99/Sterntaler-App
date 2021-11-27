2a8fbe4cd4e9ce878e18ddbf47c47d48b77ae66f
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-11-28 00:13:15 +0100

    ✨ Setup HTML and CSS

--- src/pages/Notizen/notizen.tsx
+++ src/pages/Notizen/notizen.tsx
@@ -1,7 +1,26 @@
 import styles from './notizen.module.css';
 
 function Notizen() {
-  return <div className={styles.container}>Hello World 🥳</div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.btn}>Notiz erstellen</button>
+      <label className={styles.note}>
+        <div className={styles.noteheader}>
+          <img
+            className={styles.icon}
+            src="src/assets/icons8-edit (1).svg"
+            alt=""
+          />
+          <img
+            className={styles.icon}
+            src="src/assets/icons8-trash (1).svg"
+            alt=""
+          />
+        </div>
+        <textarea className={styles.notetext}></textarea>
+      </label>
+    </div>
+  );
 }
 
 export default Notizen;
