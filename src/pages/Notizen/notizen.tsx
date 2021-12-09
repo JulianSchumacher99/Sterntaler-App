b412882faf0511db0f9a44af4971f7fee1c811d6
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-12-09 10:30:11 +0100

    ✨ Basic Form Function

--- src/pages/Notizen/notizen.tsx
+++ src/pages/Notizen/notizen.tsx
@@ -1,24 +1,39 @@
+import { useState } from 'react';
+import Note from '../../Components/Note/note';
 import styles from './notizen.module.css';
 
-function Notizen() {
+function Notizen(): JSX.Element {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      text: 'This is my first note',
+    },
+    {
+      id: 2,
+      text: 'This is my second note',
+    },
+    {
+      id: 3,
+      text: 'This is my third note',
+    },
+  ]);
+
+  function onAddNote() {
+    console.log('Add');
+    console.log();
+  }
+
   return (
     <div className={styles.container}>
-      <button className={styles.btn}>Notiz erstellen</button>
-      <label className={styles.note}>
-        <div className={styles.noteheader}>
-          <img
-            className={styles.icon}
-            src="src/assets/icons8-edit (1).svg"
-            alt=""
-          />
-          <img
-            className={styles.icon}
-            src="src/assets/icons8-trash (1).svg"
-            alt=""
-          />
-        </div>
-        <textarea className={styles.notetext}></textarea>
-      </label>
+      <button className={styles.btn} onClick={onAddNote}>
+        Notiz erstellen
+      </button>
+
+      {notes.map((note) => (
+        // eslint-disable-next-line react/jsx-key
+        <Note id={note.id} text={note.text} />
+      ))}
     </div>
   );
 }
